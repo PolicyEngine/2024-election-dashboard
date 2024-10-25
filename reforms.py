216a409c7127ae8b6b6cf336ea029053f7806da4
PavelMakarchuk <pavel.ma99@gmail.com>
2024-10-26 01:47:17 +0700

    reform

--- reforms.py
+++ reforms.py
@@ -82,4 +82,22 @@ COMBINED_REFORMS = {
             "2024-01-01.2100-12-31": 0
         },
         "gov.irs.social_security.taxability.rate.base": {"2024-01-01.2100-12-31": 0},
+        },
+        # Trump Tax Exemptions
+        "gov.contrib.tax_exempt.in_effect": {
+            "2025-01-01.2100-12-31": True
+        },
+        "gov.contrib.tax_exempt.overtime.income_tax_exempt": {
+            "2025-01-01.2100-12-31": True
+        },
+        "gov.contrib.tax_exempt.overtime.payroll_tax_exempt": {
+            "2025-01-01.2100-12-31": True
+        },
+        "gov.contrib.tax_exempt.tip_income.income_tax_exempt": {
+            "2025-01-01.2100-12-31": True
+        },
+        "gov.contrib.tax_exempt.tip_income.payroll_tax_exempt": {
+            "2025-01-01.2100-12-31": True
+    }
 }
+
